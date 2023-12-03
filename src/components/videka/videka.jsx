3b9c03ad2879b5836b706f8ca83d4aea76ee1641
AlexanderK-coder <as3608@eyandex.ru>
2023-12-03 22:54:10 +0300

    cards_css

--- src/components/videka/videka.jsx
+++ src/components/videka/videka.jsx
@@ -3,42 +3,54 @@ import React from "react";
 // import Kams from "./Kamni2.jpg";
 // import Kamk from "./kamni3.jpg";
 // import "./kamni1.jpg";
-// import "./Kamni2.jpg";
+// import kamni2 from "./Kamni2.jpg";
 // import "./kamni3.jpg";
+import "./video.css";
 
 const elements = [
   {
     id: 1,
-    foto: require("./kamni1.jpg"),
+    foto: require("./vid.mp4"),
     title: "Весна",
     singer: "Роман",
+    poster: require("./kamni1.jpg"),
   },
   {
     id: 2,
-    foto: require("./Kamni2.jpg"),
+    foto: require("./vid.mp4"),
     title: "Зима",
     singer: "Иван",
+    poster: require("./Kamni2.jpg"),
   },
   {
     id: 3,
-    foto: require("./kamni3.jpg"),
+    foto: require("./vid.mp4"),
     title: "Осень",
     singer: "Петро",
+    poster: require("./kamni3.jpg"),
   },
 ];
 
 export function Videka() {
   return (
-    <>
+    <div className="elements">
       {elements.map((element) => {
         return (
           <div key={element.id} className="element">
-            <img src={element.foto} width="300px" height="150px" alt=""></img>
+            {/* <img src={element.foto} width="300px" height="150px" alt=""></img>
+             */}
+            <video
+              src={element.foto}
+              controls
+              width="300px"
+              height="150px"
+              poster={element.poster}
+            ></video>
             <h1>{element.title}</h1>
             <p>{element.singer}</p>
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
