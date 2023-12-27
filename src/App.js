cb0bf221acf68afab54be9da4b4f63586e599d97
AlexanderK-coder <as3608@eyandex.ru>
2023-12-27 12:33:36 +0300

    пром

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
+      <div className="container flexelements">
         <Header />
+        <article></article>
         <Footer />
       </div>
     </BrowserRouter>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Stack } from "react-bootstrap";
 import "./footer.css";
 
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <Stack direction="horizontal" gap={2}>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+      </Stack>
+      ;
+    </>
+  );
 };
 
 export default Footer;
