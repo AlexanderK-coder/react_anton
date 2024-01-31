d6f4d3017912b653d839f6dc9cedc9228516bf64
AlexanderK-coder <as3608@eyandex.ru>
2024-01-31 13:29:17 +0300

    grid+card

--- src/pages/music/Music.jsx
+++ src/pages/music/Music.jsx
@@ -1,11 +1,116 @@
 import React from "react";
 import "./music.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+// import CardGroup from "react-bootstrap/CardGroup";
 
 const Music = () => {
   return (
     <>
-      <div className="music">
-        <p>Это музыкальная страница</p>
+      <div className="section">
+        <div className="cont">
+          <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
+            {Array.from({ length: 6 }).map((idx) => (
+              <Col key={idx}>
+                <Card>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </Card.Footer>
+                </Card>
+                {/* <Card>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                      This card has supporting text below as a natural lead-in
+                      to additional content.{" "}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </Card.Footer>
+                </Card>
+                <Card>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This card has even
+                      longer content than the first to show that equal height
+                      action.
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </Card.Footer>
+                </Card> */}
+              </Col>
+            ))}
+          </Row>
+          {/* <CardGroup>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This card has even longer
+                  content than the first to show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardGroup> */}
+        </div>
       </div>
     </>
   );
